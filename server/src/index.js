40e08a61fdb9716d92b80286d3f086141885f6c1
sohelreza <rezasmsohel@gmail.com>
2026-03-07 05:27:58 +0600

    refactor: switch to ES module imports

--- server/src/index.js
+++ server/src/index.js
@@ -1,6 +1,8 @@
-const express = require("express");
-const cors = require("cors");
-require("dotenv").config();
+import cors from "cors";
+import dotenv from "dotenv";
+import express from "express";
+
+dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5000;
